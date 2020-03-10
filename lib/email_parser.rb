3561b8cc65d8a1ad85db1cdca451d9a8f20de34d
John Souza <souzajohnlee@gmail.com>
2020-03-10 21:27:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailAddressParser
   end
   
   def parse
-    @emails.split(" ").each {|email email.tr!(",", "")}.uniq
+    @emails.split(" ").each {|email| email.tr!(",", "")}.uniq
   end
   
 end
