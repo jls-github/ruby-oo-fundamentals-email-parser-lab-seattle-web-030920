ecff577d1e056d0cf3e97dbfb2b8f82eee0987be
John Souza <souzajohnlee@gmail.com>
2020-03-10 21:16:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailAddressParser
   end
   
   def parse
-    @emails.split(" ").each {|email| split(",").reject {|email| email == ","}}
+    @emails.split(" ").each {|email| emailsplit(",").reject {|text_group| text_group == ","}}
   end
   
 end
